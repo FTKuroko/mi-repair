0b290ee57353294d50695ca88b41d62eda8245b4
Kuroko <982239116@qq.com>
2024-05-13 12:51:12 +0800

    init

---
+++ mi_repair-server/src/main/java/com/mi_repair/MiRepairServerApplication.java
@@ -0,0 +1,13 @@
+package com.mi_repair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiRepairServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiRepairServerApplication.class, args);
+    }
+
+}
---
+++ mi_repair-server/src/test/java/com/mi_repair/MiRepairServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mi_repair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiRepairServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
