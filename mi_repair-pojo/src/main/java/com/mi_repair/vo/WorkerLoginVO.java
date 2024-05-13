556c3940e9c72cda0eaf88808676b0516d05148b
Kuroko <982239116@qq.com>
2024-05-13 21:55:45 +0800

    统一接口输入输出数据格式

---
+++ mi_repair-pojo/src/main/java/com/mi_repair/vo/WorkerLoginVO.java
@@ -0,0 +1,11 @@
+package com.mi_repair.vo;
+
+import java.io.Serializable;
+
+/**
+ * @author Kuroko
+ * @description
+ * @date 2024/5/13 21:43
+ */
+public class WorkerLoginVO implements Serializable {
+}
