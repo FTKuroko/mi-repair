8ff0e5465cd67e1522cff27ad761ae0253227bd0
Kuroko <982239116@qq.com>
2024-05-14 16:18:59 +0800

    对登录注册代码稍微做了点调整，统一规范，mapper只扫描实体类，不扫描DTO、VO对象。在代码中自行将DTO数据转换成对应的实体类数据

--- mi_repair-server/src/main/java/com/mi_repair/service/impl/UserServiceImpl.java
+++ mi_repair-server/src/main/java/com/mi_repair/service/impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.mi_repair.entity.User;
 import com.mi_repair.mapper.UserMapper;
 import com.mi_repair.service.UserService;
 import com.mi_repair.vo.UserLoginVO;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,9 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserLoginVO login(UserLoginDTO userLoginDTO) {
-        User login = userMapper.login(userLoginDTO);
+        User user = new User();
+        BeanUtils.copyProperties(userLoginDTO, user);
+        User login = userMapper.login(user);
         UserLoginVO vo = new UserLoginVO();
         if(login!=null) vo.setName(login.getName());
         return vo;
@@ -29,7 +32,9 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserLoginVO register(UserRegDTO dto) {
-        userMapper.register(dto);
+        User user = new User();
+        BeanUtils.copyProperties(dto, user);
+        userMapper.register(user);
         UserLoginVO vo = new UserLoginVO();
         vo.setName(dto.getName());
         return dto.getId()==null?null:vo;
