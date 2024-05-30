8fe06a230bd9b3b655af3be7cfbfcf9a81a3699b
笙箫语陌 <3453479469@qq.com>
2024-05-30 20:41:37 +0800

    归还设备接口

--- mi-repair-server/src/main/java/com/mi/repair/service/impl/OrderRepairServiceImpl.java
+++ mi-repair-server/src/main/java/com/mi/repair/service/impl/OrderRepairServiceImpl.java
@@ -220,6 +220,24 @@ public class OrderRepairServiceImpl implements OrderRepairService {
     }
 
     /**
+     * 归还设备
+     * @param id
+     * @return
+     */
+    @Override
+    public int returnDevice(Long id) {
+        OrderRepair orderRepair = orderRepairMapper.selectById(id);
+        // 2、 获取当前工程师 id
+        Long workerId = BaseContext.getCurrentId();
+        // 3、 判断当前维修单是否属于该工程师以及维修单当前状态是否为维修状态
+        if(workerId.equals(orderRepair.getWorkerId()) && orderRepair.getStatus().equals(RepairOrderStatus.REPAIR.getCode())){
+            scheduleService.insertSchedule(id,RepairOrderStatus.DONE.getCode(),1);
+            return orderRepairMapper.updateStatusById(id,RepairOrderStatus.DONE.getCode());
+        }
+        return 0;
+    }
+
+    /**
      * 只申请一种材料
      * @param repairMaterialsDTO
      * @return
